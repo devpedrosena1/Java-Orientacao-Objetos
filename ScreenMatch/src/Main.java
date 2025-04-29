65ce55c9f29fb77b4e6117e8e9dbe494b9cdc6fe
devpedrosena1 <devpedrosena1@gmail.com>
2025-04-29 17:22:16 -0300

    Entendendo sobre como instanciar um objeto

--- ScreenMatch/src/Main.java
+++ ScreenMatch/src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // instanciar objeto
+        Filme meuFilme = new Filme();
+        meuFilme.anoDeLancamento = 2019;
+        meuFilme.nome = "Creed";
+        meuFilme.avaliacao = 9.6;
+
+        System.out.println(meuFilme.anoDeLancamento);
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.avaliacao);
+
     }
 }
