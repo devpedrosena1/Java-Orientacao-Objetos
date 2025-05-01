829ef5b46c7916b0622702df5121a7d1a15ef17a
devpedrosena1 <devpedrosena1@gmail.com>
2025-05-01 15:19:56 -0300

    Aprendendo sobre pacotes, getters e setters.

--- ScreenMatch/src/Main.java
+++ ScreenMatch/src/Main.java
@@ -1,18 +1,20 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Main {
     public static void main(String[] args) {
 
         // instanciar objeto
         Filme meuFilme = new Filme();
-        meuFilme.anoDeLancamento = 2019;
-        meuFilme.nome = "Creed";
-        meuFilme.somaDasAvaliacoes = 9.6;
 
-        meuFilme.exibreFichaTecnica();
+        meuFilme.setNome("Creed");
+        meuFilme.setAnoDeLancamento(2019);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
 
         meuFilme.avaliaFilme(8.90);
         meuFilme.avaliaFilme(9.70);
 
-        System.out.println("Soma das avaliações: " + meuFilme.somaDasAvaliacoes);
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
 
         System.out.println("Média das avaliações: " + meuFilme.pegaMedia());
---
+++ ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,65 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+
+    // visibilidade, tipos de dados e atributos
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    // getters (retornar \ exibir) e setters (entradas)
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    // metodos
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme:" + nome);
+        System.out.println("Ano de lançamento:" + anoDeLancamento);
+    }
+
+    public void avaliaFilme(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        double media = somaDasAvaliacoes / totalDeAvaliacoes;
+        return media;
+    }
+}
