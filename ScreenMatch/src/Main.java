ca632d7bea2c60f1ae1a4a16f90ac1d8518a6a46
devpedrosena1 <devpedrosena1@gmail.com>
2025-05-06 13:35:46 -0300

    Aprendendo mais sobre herança

--- ScreenMatch/src/Main.java
+++ ScreenMatch/src/Main.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,15 +10,22 @@ public class Main {
         meuFilme.setNome("Creed");
         meuFilme.setAnoDeLancamento(2019);
         meuFilme.setDuracaoEmMinutos(180);
-
         meuFilme.exibeFichaTecnica();
 
         meuFilme.avaliaFilme(8.90);
         meuFilme.avaliaFilme(9.70);
 
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
-
         System.out.println("Média das avaliações: " + meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("O tempo em minutos para maratonar a série " + lost.getNome() + " é: " + lost.getDuracaoEmMinutos() + " minutos");
 
     }
 }
--- ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,65 +1,16 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
+public class Filme extends Titulo {
 
-    // visibilidade, tipos de dados e atributos
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+    private String diretor;
 
-    // getters (retornar \ exibir) e setters (entradas)
-    public String getNome() {
-        return nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
 
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    // metodos
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme:" + nome);
-        System.out.println("Ano de lançamento:" + anoDeLancamento);
-    }
-
-    public void avaliaFilme(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        double media = somaDasAvaliacoes / totalDeAvaliacoes;
-        return media;
-    }
 }
