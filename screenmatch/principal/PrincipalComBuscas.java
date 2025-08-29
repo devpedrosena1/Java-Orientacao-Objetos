e4972d65bbce4fa87636f85fc3e5cbd8d32432de
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-29 08:48:02 -0300

    consumindo api o odbm, filmes

--- screenmatch/principal/PrincipalComBuscas.java
+++ screenmatch/principal/PrincipalComBuscas.java
@@ -1,18 +1,31 @@
 package br.com.alura.screenmatch.principal;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBuscas {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = scanner.nextLine();
+
+        var endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=b49e11ef";
 
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=divertidamente&apikey=b49e11ef"))
+                .uri(URI.create(endereco)
                 .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
 
     }
 
